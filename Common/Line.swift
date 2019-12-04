ec9dd5cd66e1e77eb31a177a67189292ae6ef62a
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2019-12-03 22:26:31 -0500

    Create Line type

---
+++ Common/Line.swift
@@ -0,0 +1,52 @@
+//
+//  Line.swift
+//  Common
+//
+//  Created by Marc-Antoine Malépart on 2019-12-03.
+//  Copyright © 2019 Marc-Antoine Malépart. All rights reserved.
+//
+
+import Foundation
+
+public struct Line {
+    public var start: Point
+    public var end: Point
+    
+    public init(start: Point, end: Point) {
+        self.start = start
+        self.end = end
+    }
+    
+    public var deltaX: Float {
+        return end.x - start.x
+    }
+    
+    public var deltaY: Float {
+        return end.y - start.y
+    }
+    
+    public func intersection(with other: Line) -> Point? {
+        let determinant = self.deltaX * other.deltaY - self.deltaY * other.deltaX
+        
+        if abs(determinant) < 0.0001 {
+            return nil
+        }
+        
+        let ab = ((self.start.y - other.start.y) * other.deltaX - (self.start.x - other.start.x) * other.deltaY) / determinant
+        
+        guard ab > 0, ab < 1 else {
+            return nil
+        }
+        
+        let cd = ((self.start.y - other.start.y) * self.deltaX - (self.start.x - other.start.x) * self.deltaY) / determinant
+        
+        guard cd > 0, cd < 1 else {
+            return nil
+        }
+        
+        return Point(
+            x: self.start.x + ab * self.deltaX,
+            y: self.start.y + ab * self.deltaY
+        )
+    }
+}
