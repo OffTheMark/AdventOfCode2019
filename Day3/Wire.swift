047308bedd922368f98e358966ebb2bc8e511e0f
Marc-Antoine Malépart <marc-antoine.malepart@desjardins.com>
2019-12-04 13:00:28 -0500

    Reindent file

--- Day3/Wire.swift
+++ Day3/Wire.swift
@@ -13,38 +13,38 @@ import Geometry
 
 struct Wire {
     let segments: [Line]
-
+    
     init(segments: [Line]) {
         self.segments = segments
     }
-
+    
     init(origin: Point, moves: [Move]) {
         var segments = [Line]()
         var currentPoint = origin
-
+        
         for move in moves {
             let start = currentPoint
             let end = currentPoint.applying(move)
             let segment = Line(start: start, end: end)
-
+            
             segments.append(segment)
             currentPoint = end
         }
-
+        
         self.init(segments: segments)
     }
-
+    
     func steps(to point: Point) -> Float? {
         guard let firstIndex = segments.firstIndex(where: { $0.intersects(with: point) }) else {
             return nil
         }
-
+        
         let stepsInPreviousSegments = segments[0..<firstIndex]
-        .reduce(0, { result, line in
-            return result + line.length
-        })
+            .reduce(0, { result, line in
+                return result + line.length
+            })
         let stepsInIntersectingSegment = segments[firstIndex].start.linearDistance(to: point)
-
+        
         return stepsInPreviousSegments + stepsInIntersectingSegment
     }
 }
