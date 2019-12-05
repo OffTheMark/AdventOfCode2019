3ff70be2ed1f9593899991bbbba9614710e1c48e
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2019-12-04 19:05:25 -0500

    Reindent file

--- Day4/Password.swift
+++ Day4/Password.swift
@@ -10,76 +10,76 @@ import Foundation
 
 struct Password: RawRepresentable {
     let rawValue: String
-
+    
     init?(rawValue: String) {
         let digits = rawValue.compactMap({ character in
             return Int(String(character))
         })
-
+        
         guard digits.count == 6 else {
             return nil
         }
-
+        
         let digitPairs = digits.indices
             .dropLast()
             .map({ index in
-            return DigitPair(left: digits[index], right: digits[index + 1])
+                return DigitPair(left: digits[index], right: digits[index + 1])
             })
-
-
+        
+        
         var containsDouble = false
         let digitsAreAscending = digitPairs.reduce(into: true, { result, pair in
             if pair.left > pair.right {
                 result = false
             }
-
+            
             if pair.left == pair.right {
                 containsDouble = true
             }
         })
-
+        
         guard digitsAreAscending, containsDouble else {
             return nil
         }
-
+        
         self.rawValue = rawValue
     }
 }
 
 struct StricterPassword: RawRepresentable {
     let rawValue: String
-
+    
     init?(rawValue: String) {
         let digits = rawValue.compactMap({ character in
             return Int(String(character))
         })
-
+        
         guard digits.count == 6 else {
             return nil
         }
-
+        
         let digitPairs = digits.indices
             .dropLast()
             .map({ index in
                 return DigitPair(left: digits[index], right: digits[index + 1])
             })
-
+        
         guard digitPairs.allSatisfy({ $0.left <= $0.right }) else {
             return nil
         }
-
+        
         let countsPerDouble: [DigitPair: Int] = digitPairs.reduce(into: [:], { result, pair in
             guard pair.left == pair.right else {
                 return
             }
-
+            
             result[pair, default: 0] += 1
         })
-
+        
         guard countsPerDouble.contains(where: { _, count in return count == 1 }) else {
             return nil
         }
-
+        
         self.rawValue = rawValue
     }
 }
