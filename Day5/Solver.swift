eb2d9befe953380f1e46a89a96416d5f933b0be5
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2019-12-05 22:23:47 -0500

    Add complete Day 5 solutions

---
+++ Day5/Solver.swift
@@ -0,0 +1,36 @@
+//
+//  Solver.swift
+//  Day5
+//
+//  Created by Marc-Antoine Malépart on 2019-12-05.
+//  Copyright © 2019 Marc-Antoine Malépart. All rights reserved.
+//
+
+import Foundation
+import Common
+
+final class Part1Solver: Solver {
+    let program: [Int]
+    
+    init(program: [Int]) {
+        self.program = program
+    }
+    
+    func solve() throws {
+        let computer = Computer(program: program, input: 1)
+        try computer.run()
+    }
+}
+
+final class Part2Solver: Solver {
+    let program: [Int]
+    
+    init(program: [Int]) {
+        self.program = program
+    }
+    
+    func solve() throws {
+        let computer = ThermalRadiatorComputer(program: program, input: 5)
+        try computer.run()
+    }
+}
